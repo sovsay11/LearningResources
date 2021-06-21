a2b3c4ab10f937c613ba002590b6b62c9f0a351a
Noah <sovannarasay11@gmail.com>
2021-06-21 11:54:16 -0600

    Added Python.py script for learning

---
+++ python.py
@@ -0,0 +1,13 @@
+# Dictionaries
+# Creating a dict is fairly simple, just use curly braces
+thisDict = {
+    "brand": "Ford", # items are key-value pairs, with : as the separator
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(thisDict)
+
+# this grabs the length of the dictionary
+print(len(thisDict))
+
