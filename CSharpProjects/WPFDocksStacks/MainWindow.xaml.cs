a8fe7b7e40996e4bb357a45276af2b6c20fe2854
Noah <sovannarasay11@gmail.com>
2021-10-20 09:10:36 -0600

    Adds WPF text binding functionality

--- CSharpProjects/WPFDocksStacks/MainWindow.xaml.cs
+++ CSharpProjects/WPFDocksStacks/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,57 @@ namespace WPFDocksStacks
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
-  public partial class MainWindow : Window
+  public partial class MainWindow : Window, INotifyPropertyChanged
   {
+    private string _searchText;
+
+    public string SearchText
+    {
+      get { return _searchText; }
+      set
+      {
+        _searchText = value;
+
+        OnPropertyChanged("SearchText");
+        OnPropertyChanged("MyFilteredItems");
+      }
+    }
+
+    public List<string> MyItems { get; set; }
+
+    public IEnumerable<string> MyFilteredItems
+    {
+      get
+      {
+        if (SearchText == null) return MyItems;
+
+        return MyItems.Where(x => x.ToUpper().StartsWith(SearchText.ToUpper()));
+      }
+    }
+
+
     public MainWindow()
     {
       InitializeComponent();
+
+      MyItems = new List<string>() { "ABC", "DEF", "GHI" };
+
+      
+
+      this.DataContext = this;
+    }
+
+    public ICommand EnterKeyCommand
+    {
+      get;
+      private set;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    void OnPropertyChanged(string name)
+    {
+      if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
     }
   }
 }
